d9b5c2869e52b60375a4f07340bc7b8ba47b26b2
chaeys2000 <yunseoki0412@daum.net>
2023-03-31 00:46:55 +0900

    hi

--- session7/채윤석/CountProject/CountApp/views.py
+++ session7/채윤석/CountProject/CountApp/views.py
@@ -8,8 +8,6 @@ def result(request):
      text=request.POST['text']
      total_len=len(text)
      no_blank_len=len(text.replace(' ',''))
-     return render(request, 'result.html', {
-          'text':text,
-          'total_len': total_len,
-          'no_blank_len': no_blank_len})
+     gong=len(text.split())
+     return render(request, 'result.html', {'text':text,'total_len': total_len,'no_blank_len': no_blank_len,'gong': gong})
 
